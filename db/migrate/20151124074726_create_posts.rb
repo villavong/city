7011afcbed68c40ac34c5c226bef4b389e3057c3
villavong <asyao@naver.com>
2015-11-24 16:52:16 +0900

    Add Post model & associations

---
+++ db/migrate/20151124074726_create_posts.rb
@@ -0,0 +1,12 @@
+class CreatePosts < ActiveRecord::Migration
+  def change
+    create_table :posts do |t|
+      t.string :title
+      t.text :description
+      t.references :student, index: true, foreign_key: true
+
+      t.timestamps null: false
+    end
+    add_foreign_key :posts, :students
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151124025943) do
+ActiveRecord::Schema.define(version: 20151124074726) do
+
+  create_table "posts", force: :cascade do |t|
+    t.string   "title"
+    t.text     "description"
+    t.integer  "student_id"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
+
+  add_index "posts", ["student_id"], name: "index_posts_on_student_id"
 
   create_table "students", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
